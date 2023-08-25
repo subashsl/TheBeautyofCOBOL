90b9090fcc6309715f4ae5011d15f8105d402256
subashsl <143191826+subashsl@users.noreply.github.com>
2023-08-25 20:12:10 +0530

    Add files via upload
    The ubiquitous "Hello World" program, now in COBOL.

---
+++ HELLOWORLD.cbl
@@ -0,0 +1,14 @@
+      ******************************************************************
+      * Author:SUBASH S L
+      * Date: 25-Aug-2023
+      * Purpose: Program that displays the message "Hello Word!" on screen
+      * A typical first program while learning any new programming language
+      *
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLOWORLD.
+
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+           DISPLAY "Hello World!".
+           STOP RUN.
